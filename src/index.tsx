0ba4ab2dabb61b09c7f6e80febbe57bf26700d40
Mohammad <mohammadidehgamwala5@gmail.com>
2023-09-06 21:48:17 +0530

    removed container

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,3 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
 import { ZoiqThemeProvider, useZoiqTheme } from "./context/ZoiqThemeProvider";
 
 export { ZoiqThemeProvider, useZoiqTheme };
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
