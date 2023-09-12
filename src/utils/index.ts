8d823a731766f2fdea226ae18ec1d4d392924589
Mohammad <mohammadidehgamwala5@gmail.com>
2023-09-12 15:42:09 +0530

    --

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,45 +22,49 @@ const DEFAULT_COLORS = {
 };
 
 const getTheme = (styleGuideData: StyleGuide[]) => {
-  const styleGuide: any = {};
+  try {
+    const styleGuide: any = {};
 
-  styleGuideData.forEach((d) => {
-    styleGuide[d.name] = d.value;
-  });
+    styleGuideData.forEach((d) => {
+      styleGuide[d.name] = d.value;
+    });
 
-  const styleGuideValues: StyleGuideValues = styleGuide;
+    const styleGuideValues: StyleGuideValues = styleGuide;
 
-  const token: ThemeConfig["token"] = {
-    colorPrimary: styleGuideValues.primary ?? DEFAULT_COLORS.primary,
-    colorBgLayout: styleGuideValues.darkest ?? DEFAULT_COLORS.dark,
-    borderRadius: 16,
-    fontFamily: styleGuideValues?.fontFamilyPrimary ?? "Poppins, sans-serif",
-    fontSize: Number(styleGuideValues?.fontSizeBody ?? 16),
-    fontSizeHeading1: Number(styleGuideValues?.fontSizeBanner ?? 48),
-    fontSizeHeading2: Number(styleGuideValues?.fontSizeTitle ?? 32),
-    fontSizeHeading3: Number(styleGuideValues?.fontSizeSubtitle ?? 24),
-    fontSizeHeading4: Number(styleGuideValues?.fontSizeBody ?? 16),
-    lineHeightHeading1: Number(styleGuideValues?.lineHeightBanner ?? 1.2),
-    lineHeightHeading2: Number(styleGuideValues?.lineHeightTitle ?? 1.2),
-    lineHeightHeading3: Number(styleGuideValues?.lineHeightSubtitle ?? 1.2),
-    lineHeightHeading4: Number(styleGuideValues?.lineHeightBody ?? 1.2),
-    colorBorder: styleGuideValues.dark,
-    colorLink: styleGuideValues.primary ?? DEFAULT_COLORS.primary,
-  };
+    const token: ThemeConfig["token"] = {
+      colorPrimary: styleGuideValues.primary ?? DEFAULT_COLORS.primary,
+      colorBgLayout: styleGuideValues.darkest ?? DEFAULT_COLORS.dark,
+      borderRadius: 16,
+      fontFamily: styleGuideValues?.fontFamilyPrimary ?? "Poppins, sans-serif",
+      fontSize: Number(styleGuideValues?.fontSizeBody ?? 16),
+      fontSizeHeading1: Number(styleGuideValues?.fontSizeBanner ?? 48),
+      fontSizeHeading2: Number(styleGuideValues?.fontSizeTitle ?? 32),
+      fontSizeHeading3: Number(styleGuideValues?.fontSizeSubtitle ?? 24),
+      fontSizeHeading4: Number(styleGuideValues?.fontSizeBody ?? 16),
+      lineHeightHeading1: Number(styleGuideValues?.lineHeightBanner ?? 1.2),
+      lineHeightHeading2: Number(styleGuideValues?.lineHeightTitle ?? 1.2),
+      lineHeightHeading3: Number(styleGuideValues?.lineHeightSubtitle ?? 1.2),
+      lineHeightHeading4: Number(styleGuideValues?.lineHeightBody ?? 1.2),
+      colorBorder: styleGuideValues.dark,
+      colorLink: styleGuideValues.primary ?? DEFAULT_COLORS.primary,
+    };
 
-  document.documentElement.style.setProperty(
-    "--font-primary",
-    styleGuideValues.fontFamilyPrimary ?? "Poppins, sans-serif"
-  );
-  document.documentElement.style.setProperty(
-    "--font-secondary",
-    styleGuideValues.fontFamilySecondary ?? "Architects Daughter, cursive"
-  );
+    document.documentElement.style.setProperty(
+      "--font-primary",
+      styleGuideValues.fontFamilyPrimary ?? "Poppins, sans-serif"
+    );
+    document.documentElement.style.setProperty(
+      "--font-secondary",
+      styleGuideValues.fontFamilySecondary ?? "Architects Daughter, cursive"
+    );
 
-  return {
-    token,
-    styleGuideValues,
-  };
+    return {
+      token,
+      styleGuideValues,
+    };
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export { getTheme };
